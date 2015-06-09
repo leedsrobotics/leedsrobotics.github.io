d08a250674e224d18374f4754d8b5be446255d1d
leedsrobotics <tripledealkevin@live.com>
2015-06-09 15:36:30 +0100

    Update blocks.js

--- blocks.js
+++ blocks.js
@@ -1,19 +1,21 @@
-(function(ext) {
+(function(ext) 
+{
 	ext._shutdown = function() {};
 
-	ext._getStatus = function() {
+	ext._getStatus = function() 
+	{
 		return {status:2, msg: 'Ready'};
 	};
 
-	ext.wait_random = function(callback) {
+	ext.wait_random = function(callback) 
+	{
 		wait = Math.random();
 		console.log('Waiting for ' + wait + ' seconds');
-		window.setTimeout(function() {
-			callback();
-		}, wait*1000);
+		window.setTimeout(function() { callback(); }, wait*1000);
 	};
 
-	ext.power = function(base, exponent) {
+	ext.power = function(base, exponent) 
+	{
 		return Math.pow(base,exponent);	
 	};
 
@@ -31,12 +33,29 @@
 		});
 	};
 
+	ext.set_alarm = function(time) 
+	{
+		window.setTimeout(function() {alarm_went_off = true;}, time * 1000);		
+	};
+
+	ext.when_alarm = function()
+	{
+		if (alarm_went_off === true)
+		{
+			alarm_went_off = false;
+			return true;
+		}
+		return false;
+	};
+
 	var descriptor = {
 		blocks: [
 			 ['w', 'wait for random time', 'wait_random'],
 			 ['r', '%n ^ %n', 'power', 2, 3],
 			 ['R', 'current temperature in city %s', 'get_temp', 'Boston, MA'],
-		]
+			 ['', 'run alarm after %n seconds', 'set_alarm', '2'],
+			 ['h', 'when alarm goes off', 'when_alarm'],
+			]
 	};
 
 	ScratchExtensions.register('Sample extension', descriptor, ext);
