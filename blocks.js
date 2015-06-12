e3fe0ca252a134dc7e16911adb242926e6d99c0b
leedsrobotics <tripledealkevin@live.com>
2015-06-12 11:47:59 +0100

    Update blocks.js

--- blocks.js
+++ blocks.js
@@ -6,41 +6,11 @@
 		return {status:2, msg: 'Ready'};
 	};
 	
-	var poller = null;
-        ext._deviceConnected = function(dev) {
-                /**
-                 * Opens device connection, collecting device data over repeated
-                 * intervals
-                 */
-                 
-                if(device) return;
-
-                device = dev;
-                device.open();
-
-                poller = setInterval(function() {
-                        rawData = device.read();
-                }, 20);
-        };
-	
-	ext._deviceRemoved = function(dev) {
-	        /**
-	         * Closes device connection on device removal
-	         */
-	         
-                if(device != dev) return;
-                if(poller) poller = clearInterval(poller);
-                device = null;
-        };
 	
 	ext._shutdown = function() {
 	        /**
 	         * Shuts down connected devices on extension shutdown
 	         */
-	         
-                if(poller) poller = clearInterval(poller);
-                if(device) device.close();
-                device = null;
         }
 
 	ext.wait_random = function(callback) 
@@ -118,9 +88,7 @@
 	};
 
         
-        // Creates object containing device info
-        var hid_info = {type: 'hid', vendor: 0x045e, product: 0x00cb};
         
         // Register Extension
-	ScratchExtensions.register('Practise Extensions', descriptor, ext, hid_info);
+	ScratchExtensions.register('Practise Extensions', descriptor, ext);
 })({});
