65acb7feeb2add7c714b12b629516643b49a58a2
leedsrobotics <tripledealkevin@live.com>
2015-06-13 17:51:10 +0100

    Update controllerblock.js

--- controllerblock.js
+++ controllerblock.js
@@ -72,5 +72,5 @@ new (function() {
             joystickPart: ['leftX', 'leftY', 'rightX', 'rightY']
         }
     };
-    ScratchExtensions.register('Joystick', descriptor, ext, {type: 'hid', vendor:0x045e, product:0x028e});
+    ScratchExtensions.register('Joystick', descriptor, ext, {type: 'hid', vendor:0x045e, product:0x02d1});
 })();
