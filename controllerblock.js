d3408b0473d3da0cea71fd681edf6fa04f1be8c7
leedsrobotics <tripledealkevin@live.com>
2015-06-15 15:17:08 +0100

    Update controllerblock.js

--- controllerblock.js
+++ controllerblock.js
@@ -54,6 +54,7 @@ new (function() {
         var b = "llo";
         alert(a+b);
         alert(b+a);
+        alert(input[0]);
         //var retval = null;
         //var controls = '';
         //for(i = 0; i < input.length; ++i)
