afa48c998f44ac2ceb01d726c6ae155c1312a3e5
leedsrobotics <tripledealkevin@live.com>
2015-06-15 17:37:52 +0100

    Update controllerblock.js

--- controllerblock.js
+++ controllerblock.js
@@ -52,6 +52,11 @@ new (function() {
     ext.readJoystick = function() {
         alert(typeof input);
         var buffer = new ArrayBuffer(16);
+        if (buffer.byteLength === 16) {
+            alert("Yes, it's 16 bytes.");
+        } else {
+            alert("Oh no, it's the wrong size!");
+        }
         var int32View = new Int32Array(buffer);
         for (var i = 0; i < int32View.length; i++) {
             int32View[i] = i * 2;
