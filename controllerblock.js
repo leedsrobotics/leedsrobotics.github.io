8cfe268e8f54e12921a0fe55d7f862133239d537
leedsrobotics <tripledealkevin@live.com>
2015-06-16 10:28:03 +0100

    Update controllerblock.js

--- controllerblock.js
+++ controllerblock.js
@@ -51,7 +51,6 @@ new (function() {
     function convertByteStr(byte) { return (parseInt(byte, 16) - 128) / 128; }
     ext.readJoystick = function() {
         alert(typeof input);
-        var a = input.constructor;
         var buffer = new ArrayBuffer(48);
         if (buffer.byteLength === 48) {
             alert("Yes, it's 16 bytes.");
