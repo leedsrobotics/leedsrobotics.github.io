0d1d4fd4a4e0973f39799f0b12623bf450deb96a
leedsrobotics <tripledealkevin@live.com>
2015-06-25 14:59:02 +0100

    Update traceycontroller.js

--- traceycontroller.js
+++ traceycontroller.js
@@ -120,9 +120,10 @@
   		
   		expectPinData = true;
   		device.send(view.buffer); // Send command
-  		
+  		console.log(pinData);
   		setTimeout(function(){ 
   			expectPinData = false;
+  			console.log(pinData);
   			return ((pinData[0] & 0xFF) << 8) | (pinData[1] & 0xFF);
   		}, 100);
   	}
