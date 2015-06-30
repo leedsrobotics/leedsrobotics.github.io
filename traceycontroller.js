095c00aa5a09413a62b79e342f81ed9d16caa3bd
leedsrobotics <tripledealkevin@live.com>
2015-06-30 11:29:31 +0100

    Update traceycontroller.js

--- traceycontroller.js
+++ traceycontroller.js
@@ -142,7 +142,7 @@
   		var analogVal = ((pinData[0] & 0xFF) << 8) | (pinData[1] & 0xFF);
   		console.log("Analog Val:");
   		console.log(analogVal);
-  		pinData = new Uint8Array(1);
+  		pinData = new null;
   		if(analogVal > threshold)
   		{
   			return 'black';
