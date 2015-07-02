abf050c096a788519497dea612719aa91e5122a1
leedsrobotics <tripledealkevin@live.com>
2015-07-02 09:36:59 +0100

    Update traceycontroller.js

--- traceycontroller.js
+++ traceycontroller.js
@@ -56,56 +56,6 @@
 	}
 	
 	
-	var Queue = (function () {
-
-		Queue.prototype.autorun = true;
-		Queue.prototype.running = false;
-		Queue.prototype.queue = [];
-
-		function Queue(autorun) {
-			if (typeof autorun !== "undefined") {
-				this.autorun = autorun;
-			}
-			this.queue = []; //initialize the queue
-		};
-
-		Queue.prototype.add = function (callback) {
-			var _this = this;
-			//add callback to the queue
-			this.queue.push(function () {
-				var finished = callback();
-				if (typeof finished === "undefined" || finished) {
-					//  if callback returns `false`, then you have to 
-					//  call `next` somewhere in the callback
-					_this.dequeue();
-				}
-			});
-
-			if (this.autorun && !this.running) {
-				// if nothing is running, then start the engines!
-				this.dequeue();
-			}
-
-			return this; // for chaining fun!
-		};
-
-		Queue.prototype.dequeue = function () {
-			this.running = false;
-			//get the first element off the queue
-			var shift = this.queue.shift();
-			if (shift) {
-				this.running = true;
-				shift();
-			}
-			return shift;
-		};
-
-		Queue.prototype.next = Queue.prototype.dequeue;
-
-		return Queue;
-
-	})();
-	
 	/**
 	 * Return status of the extension
 	 */
@@ -222,10 +172,10 @@
   		
   		dataRequested = new Date().getTime();
   	
-  		setTimeout(function(){
-  			var pinColour = processPinData();
-	  		return pinColour; 
-	  		}, 120);
+  		var pinColour = processPinData();
+  		
+  		sleep(120);
+	  	return pinColour; 
   		
   	}
 
