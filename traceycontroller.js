9ac4b32f438a48b1ff40ae4437d8da4baccdb27d
leedsrobotics <tripledealkevin@live.com>
2015-07-03 11:47:49 +0100

    Update traceycontroller.js

--- traceycontroller.js
+++ traceycontroller.js
@@ -107,6 +107,7 @@
         		dataView = new Uint8Array(data);
         		storedData.write(dataView);
         		dataReceived = true;
+        		console.log(dataView);
         		//console.log('Latency:');
         		//console.log(new Date().getTime() - dataRequested);
         		//console.log(storedData.expectedLength == storedData.latestElement);
