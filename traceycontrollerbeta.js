06b6003cf4ee0d8fc9f10b3cf74f20f4cca2de04
leedsrobotics <tripledealkevin@live.com>
2015-07-03 14:25:05 +0100

    Update traceycontrollerbeta.js

--- traceycontrollerbeta.js
+++ traceycontrollerbeta.js
@@ -8,7 +8,7 @@
 	var previousLeftSpeed = 0;
 	var expectPinData = false;
 	var pinData = null;
-	var threshold = 585;
+	var threshold = 465;
 	var analogLimit = 1000;
 	var dataRequested = new Date().getTime();
 	var pinVal = 0;
